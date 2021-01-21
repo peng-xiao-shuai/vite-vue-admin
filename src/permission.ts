0ab7dc675644351265eff66497c4a58000353a50
PengXiaoShuai <1612565136@qq.com>
2021-01-21 11:46:35 +0800

    新增 可添加不重定向路由

--- src/permission.ts
+++ src/permission.ts
@@ -5,19 +5,35 @@ import store from './store'
 // import { Message } from 'element-ui'
 // import { getToken } from '@/utils/auth' // 验权
 
-const whiteList = ['/login'] // 不重定向白名单
+const notRedirect = ['/login'] // 不重定向白名单
 router.beforeEach((to, from, next) => {
 
-  console.log(store.getters.getToken);
+  // 1 判断是否有token
+    // 1.1 判断是否有menus(动态路由。页面刷新后vuex会重置)
+      // 1.1.1 重新获取动态路由表
+      // 1.1.2 直接跳转 next()
+
+    // 1.2 判断是否在重定向的白名单
+      // 1.2.1 重定向到登录页
+      // 1.2.2 留在当前页
 
   if(store.getters.getToken){
-    next()
+    if(store.getters.getMenus.length == 0){
+      
+      store.dispatch('userInfo')
+      .then(res => {
+        next()
+      })
+    }else{
+      next()
+    }
   }else{
-    if(to.path === '/login'){
+    if(notRedirect.indexOf(to.path) != -1){
       next()
     }else{
-      next({ path: '/login'})
+      next('/login')
     }
+    
   }
 })
 
