2805b4aa88e0b5d2346740b640a2712da4487d6a
PengXiaoShuai <1612565136@qq.com>
2021-02-18 15:44:59 +0800

    国际化配置

--- src/main.ts
+++ src/main.ts
@@ -4,24 +4,29 @@ import router from './router/index'
 import store from './store'
 import ElementPlus from 'element-plus'
 
+// 国际化
+import 'dayjs/locale/zh-cn'
+import locale from 'element-plus/lib/locale/lang/zh-cn'
+
 import './permission'
 import './icons'
 
 // 全局组件
 import SvgIcon from './components/SvgIcon/index.vue'// svg component
+import powerfulTable from './components/powerful-table/index.vue'// svg component
 
 // 全局静态配置
 import defalutData from './config/defalut-data';
 
 // css
 // vite 暂不支持scss :export
-// import './style/index.scss';
 import './style/iconfont.css';
-import "element-plus/packages/theme-chalk/src/index.scss";
+// import "element-plus/packages/theme-chalk/src/index.scss";
+import './style/index.scss';
 
 import {mockXHR} from '../mock/index';
 
-if(process.env.NODE_ENV === 'development'){
+if(process.env.NODE_ENV === 'development' && import.meta.env.VITE_MOCK == true){
     mockXHR()
 }
 
@@ -36,9 +41,11 @@ app.mixin({
     }
 })
 
-app.use(store).use(router).use(ElementPlus).mount('#app')
+app.use(store).use(router).use(ElementPlus, { locale }).mount('#app')
 
 app.component('svg-icon',SvgIcon)
+app.component('powerful-table',powerfulTable)
+
 // 全局挂载
 
 app.config.globalProperties.defalutData=defalutData;
