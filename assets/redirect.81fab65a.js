d50cde308b315f1cb79de4361b0aff4e98b4497c
Peng-Xiao-Shuai-0902 <53845479+Peng-Xiao-Shuai-0902@users.noreply.github.com>
2021-05-17 08:25:11 +0000

    Deploying to gh-pages from  @ aacd2cf6921a7ce1970f58a29769767ba67650f0 🚀

---
+++ assets/redirect.81fab65a.js
@@ -0,0 +1,1 @@
+import{k as e,l as a}from"./index.1087f854.js";import"./vendor.a8c43f00.js";const o={setup(){let o=e(),r=a();const{params:s,query:t}=r;console.log(r),o.replace({name:s.__name,params:s,query:t})}};export default o;
