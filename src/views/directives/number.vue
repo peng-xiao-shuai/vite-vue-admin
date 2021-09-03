5dd945e42518ad5401af6278048f66a4b5d76b6e
Peng_Xiao_Shuai_0902 <1612565136@qq.com>
2021-09-03 08:46:15 +0800

    优化：数字指令页面优化

--- src/views/directives/number.vue
+++ src/views/directives/number.vue
@@ -2,7 +2,7 @@
   <el-card :shadow="defaultData.cardShadow">
     <div class="operate-container">
       <div>
-        <i class="viteIcon vitenumber" style="margin-right: 5px"></i>
+        <i :class="[defaultData.iconfont,'vitenumber']" style="margin-right: 5px"></i>
         <span>数字指令</span>
       </div>
     </div>
@@ -10,11 +10,19 @@
     <div class="grid grid-c-4">
       <div>
         <div class="tipBox">整数</div>
-        <el-input v-model="inputs.number" placeholder="" v-num></el-input>
+        <el-input v-model="inputs.number" placeholder="" v-num>
+          <template #append>
+            v-num
+          </template>
+        </el-input>
       </div>
       <div>
         <div class="tipBox">小数（默认2位）</div>
-        <el-input v-model="inputs.float" placeholder="" v-num:float></el-input>
+        <el-input v-model="inputs.float" placeholder="" v-num:float>
+          <template #append>
+            v-num:float
+          </template>
+        </el-input>
       </div>
       <div>
         <div class="tipBox">小数（自定义保留n位）</div>
@@ -22,7 +30,11 @@
           v-model="inputs.floatLength"
           placeholder=""
           v-num:float.5
-        ></el-input>
+        >
+          <template #append>
+            v-num:float.5
+          </template>
+        </el-input>
       </div>
       <div>
         <div class="tipBox">整数 || 小数（固定范围）</div>
@@ -30,7 +42,11 @@
           v-model="inputs.numberScope"
           placeholder=""
           v-num="[1, 10]"
-        ></el-input>
+        >
+          <template #append>
+            v-num="[1, 10]"
+          </template>
+        </el-input>
       </div>
     </div>
   </el-card>
