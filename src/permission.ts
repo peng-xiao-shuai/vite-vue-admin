c772b80f8079e03e315f3f8676e37df3f223b521
peng-xiao-shuai <1612565136@qq.com>
2023-03-07 20:59:55 +0800

    perf: 修改语法

--- src/permission.ts
+++ src/permission.ts
@@ -9,17 +9,15 @@ NProgress.configure({ showSpinner: false })
 
 const notRedirect = ['/login'] // 不重定向白名单
 
-router.beforeEach((to, from, next) => {
+router.beforeEach(async (to, _from) => {
   NProgress.start()
   // 拦截 baseUrl 路径
   if (
     window.location.host === 'peng-xiao-shuai.github.io' &&
     (import.meta as any).env.BASE_URL === '/vite-vue-admin/' &&
     to.path === '/vite-vue-admin/'
-  ) {
-    next('/')
-    NProgress.done()
-  }
+  )
+    return '/'
 
   // 1 判断是否有token
   // 1.1 判断是否有menus(动态路由。页面刷新后vuex会重置)
@@ -29,26 +27,20 @@ router.beforeEach((to, from, next) => {
   // 1.2 判断是否在重定向的白名单
   // 1.2.1 重定向到登录页
   // 1.2.2 留在当前页
-
   if (store.getters.getToken) {
     if (store.getters.getMenus.length == 0) {
-      store.dispatch('userInfo').then(() => {
-        next({ ...to, replace: true })
-        NProgress.done()
-      })
-    } else {
-      next()
-      NProgress.done()
-    }
-  } else {
-    setTimeout(() => {
-      notRedirect.indexOf(to.path) != -1 ? next() : next('/login')
-      NProgress.done()
-    }, 1000)
-  }
+      const isGetUser = await store.dispatch('userInfo')
+      if (isGetUser) return { ...to, replace: true }
+      else return '/login'
+    } else return true
+  } else return notRedirect.indexOf(to.path) != -1 ? true : '/login'
 })
 
 router.afterEach(() => {
   NextLoading.done()
   NProgress.done() // 结束Progress
 })
+
+// router.onError((err, to, from) => {
+//   console.log(err, to, from)
+// })
